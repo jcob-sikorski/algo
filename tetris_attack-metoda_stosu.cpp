fe506608f7535b8dabdd50c6c1009ff92beed480
jcob-sikorski <jm.siekiera@gmail.com>
2020-09-19 14:26:07 +0200

    created tetris_attack function.

--- tetris_attack-metoda_stosu.cpp
+++ tetris_attack-metoda_stosu.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <iostream>
 #include <memory>
+#include <algorithm>
 
 using namespace std;
 
@@ -100,6 +101,55 @@ class linked_list {
     }
 
 
+    void delete_node(node *n) {
+            node *pre = new node();
+            node *cur = new node();
+            cur = head;
+
+            while(cur->next != NULL && cur != n) {
+                pre = cur;
+                cur = cur->next;
+            }
+
+            pre->next = cur->next;
+            if (cur == head) {
+                head = cur->next;
+            }
+            else if (cur == tail) {
+                tail = pre;
+            }
+
+            delete cur;
+            display_nodes();
+    }
+
+
+    void delete_by_data(int data) {
+            node *pre = new node();
+            node *cur = new node();
+            cur = head;
+
+
+            while(cur->next != NULL && cur->data != data) {
+                pre = cur;
+                cur = cur->next;
+            }
+
+
+            pre->next = cur->next;
+            if (cur == head) {
+                head = cur->next;
+            }
+            else if (cur == tail) {
+                tail = pre;
+            }
+
+
+            delete cur;
+            display_nodes();
+    }
+    
+
     void delete_index(int index) {
         node *pre = new node();
         node *cur = new node();
@@ -113,7 +163,7 @@ class linked_list {
         pre->next = cur->next;
         delete cur;
     }
-    
+
 
     void display_nodes() {
         node *temp = new node();
@@ -124,6 +174,7 @@ class linked_list {
             temp = temp->next;
         }
         cout << temp->data << endl;
+        cout << endl << endl;
     }
 
 
@@ -167,6 +218,79 @@ class linked_list {
         return tail;
     }
 
+
+    static bool by_data(node *a, node *b) { 
+        return (a->data < b->data); 
+    }
+
+
+    void tetris_attack() {
+        node *t = new node();
+        node *h = new node();
+
+        node *pre = new node();
+        node *cur = new node();
+
+        h = head;
+
+        pre = h->next;
+        
+        if (pre->next != NULL) {
+            cur = pre->next;
+            t = cur->next;
+        }
+
+        int wynik = 0;
+        while (t->next != NULL) {
+            int counter = 0;
+            if (h != t) {
+                vector<node*> potential_inv {h, pre, cur, t};
+
+                sort(potential_inv.begin(), potential_inv.end(), linked_list::by_data);
+
+                for (node* a : potential_inv) {
+                    cout << a->data << "  ";
+                }
+                cout << endl;
+
+
+                node *temp = new node();
+                int i = 0;
+
+                bool altered = false;
+
+                while(i < 3) {
+                    temp = potential_inv.at(i);
+                    if (temp->data == potential_inv.at(i+1)->data) {
+                        int data1 = temp->data;
+                        int data2 = temp->next->data;
+                        
+                        for (int j = 0; j < 2; j++) {
+                            delete_by_data(data1);
+                            delete_by_data(data2);
+                            altered=true;
+                        }
+                        i++;
+                    }
+                    i++;
+                }
+                if (!altered) {
+                    h = h->next;
+                    pre = h->next;
+                    cur = pre->next;
+                    t = cur->next;
+                } else {
+                    linked_list::tetris_attack();
+                    cout << wynik << endl;
+                }
+            }
+            display_nodes();
+        }
+        return wynik;
+    }
+
+
+    //node* clone() const { return new node(*this); }
 };
 
 
@@ -183,23 +307,23 @@ class linked_list {
 
 int main()
 {
+    vector<int> arr = {5, 2, 3, 1, 4, 1, 4, 3, 5, 2};
     // example
     linked_list obj;
-	obj.insert_end(25);
-	obj.insert_end(50);
-	obj.insert_end(90);
-	obj.insert_end(40);
-    obj.insert_end(100);
-	cout<<"\n--------------------------------------------------\n";
-	cout<<"---------------Displaying All nodes---------------";
-	cout<<"\n--------------------------------------------------\n";
-	obj.display_nodes();
 
-    linked_list nd;
+    for (int i = 0; i < arr.size(); i++) {
+        obj.insert_end(arr.at(i));
+    }
 
-    nd = obj.split_index(2);
+	//obj.insert_end(25);
+	//obj.insert_end(50);
+	//obj.insert_end(90);
+	//obj.insert_end(40);
+    //obj.insert_end(100);
 
-    cout << "obj tail " << obj.return_tail()->data << "nd head " << nd.return_head()->data << endl;
+    obj.display_nodes();
+
+    obj.tetris_attack();
 
 	system("pause");
 	return 0;
